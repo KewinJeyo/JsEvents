970b8a37fdea67f6ff5540afcfa21876ee06c1c5
Kevin <kevinkjeyovjec@gmail.com>
2026-05-08 09:27:26 +0530

    css change

--- EventsDay02/script.js
+++ EventsDay02/script.js
@@ -1,2 +1,34 @@
 let arr=[10,20,30];
 console.log(arr)
+
+
+let a = [10,100,1000];
+let fst = a[0];
+
+arr.push(40);
+console.log(arr)
+arr.pop()
+console.log(arr)
+
+
+console.log("First Item: ", fst);
+
+let obj={
+    name:"Kewin",
+    age:20,
+    department:"CSD"
+
+}
+console.log(obj);
+console.log(obj.name);
+obj.name="K"
+console.log(obj.name);
+obj.Address="Kannur"
+console.log(obj);
+
+
+for (i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
+
